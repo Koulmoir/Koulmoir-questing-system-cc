3c7e4b5a1f65ea63d569bc7344872ebe2c64817e
KKoulmoir <hallonein222000@gmail.com>
2023-06-16 15:40:44 +0200

    why did my first commit not work tf

---
+++ src/main/java/com/Koulmoir/QuestingSystemAutomation/questingSystemAutomation.java
@@ -0,0 +1,46 @@
+package com.Koulmoir.QuestingSystemAutomation;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(questingSystemAutomation.MODID)
+public class questingSystemAutomation
+{
+    public static final String MODID = "questing";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+
+    public questingSystemAutomation()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+
+        }
+    }
+}
